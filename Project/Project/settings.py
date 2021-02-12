defc6390d05429156d3327ca1efa8010a0ea6864
Narasimha Sai Raja Sakhumalla <sairaja899@gmail.com>
2021-02-12 15:46:54 +0530

    created home page and made some changes to settings.py

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -115,6 +115,10 @@ USE_L10N = True
 USE_TZ = True
 
 
+import os
+MEDIA_ROOT= os.path.join(BASE_DIR,'static/images')
+MEDIA_URL='/images/'
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
